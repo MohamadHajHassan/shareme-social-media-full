5b194b581934a1eb6ab7396898d5cdaf8c7d459e
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-02-06 20:30:25 +0200

    frontend: add and style login background video

--- shareme-frontend/src/components/Login.jsx
+++ shareme-frontend/src/components/Login.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import shareVideo from "../assets/share.mp4";
 
 const Login = () => {
-    return <div>Login</div>;
+    return (
+        <div className="flex justify-start items-center flex-col h-screen">
+            <div className="relative w-full h-full">
+                <video
+                    src={shareVideo}
+                    type="video/mp4"
+                    autoPlay
+                    loop
+                    muted
+                    controls={false}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Login;
