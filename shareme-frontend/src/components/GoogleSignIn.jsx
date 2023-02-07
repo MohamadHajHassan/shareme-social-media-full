4c3cec887a392e34a84ed29d610faf3f3210d486
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-02-07 15:53:56 +0200

    frontend: connect signin to sanity

--- shareme-frontend/src/components/GoogleSignIn.jsx
+++ shareme-frontend/src/components/GoogleSignIn.jsx
@@ -2,12 +2,30 @@ import React from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { GoogleLogin } from "@react-oauth/google";
 import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 
+import { client } from "../client";
+
 const GoogleSignIn = () => {
+    const navigate = useNavigate();
+    
     const responseGoogle = response => {
         const decoded = jwt_decode(response.credential);
-        console.log(decoded);
+        localStorage.setItem("user", JSON.stringify(decoded));
+        const { name, sub, picture } = decoded;
+        console.log(name, sub, picture);
+
+        const doc = {
+            _id: sub,
+            _type: "user",
+            userName: name,
+            image: picture,
+        };
+
+        client.createIfNotExists(doc).then(() => {
+            navigate("/", { replace: true });
+        });
     };
     return (
         <GoogleOAuthProvider
