270c337502c598e98ff6d46b4ffba54f0c0e9999
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2023-02-11 23:03:49 +0200

    frontend: implement and style feed comp

--- shareme-frontend/src/components/Feed.jsx
+++ shareme-frontend/src/components/Feed.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { client } from "../client";
+import { MasonryLayout, Spinner } from "../components";
+import { searchQuery, feedQuery } from "../utils/data";
 
 const Feed = () => {
-    return <div>Feed</div>;
+    const [loading, setLoading] = useState(false);
+    const [pins, setPins] = useState(null);
+    const { catageoryId } = useParams();
+
+    useEffect(() => {
+        setLoading(true);
+        if (catageoryId) {
+            const query = searchQuery(catageoryId);
+            client.fetch(query).then(data => {
+                setPins(data);
+                setLoading(false);
+            });
+        } else {
+            client.fetch(feedQuery).then(data => {
+                setPins(data);
+                setLoading(false);
+            });
+        }
+    }, [catageoryId]);
+
+    if (loading)
+        return <Spinner message="We are adding new ideas to your feed!" />;
+
+    return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 };
 
 export default Feed;
